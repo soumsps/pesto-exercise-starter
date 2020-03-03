5aba2ada006aa342d19540aa778c60522b9c7469
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-03-03 16:10:15 +0530

    done sumAll

--- js-exercises/sum-all/sumAll.js
+++ js-exercises/sum-all/sumAll.js
@@ -1,6 +1,16 @@
-function sumAll() {
+function sum(a, b) {
+  let res = 0;
+  for (let i = a; i <= b; i += 1) {
+    res += i;
+  }
 
+  return res;
 }
+
+function sumAll(arr) {
+  return sum(Math.min(...arr), Math.max(...arr));
+}
+
 
 export {
   sumAll,
