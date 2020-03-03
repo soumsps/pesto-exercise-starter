b35041d0a568997f23dcf1bb70d1150ad3b9e307
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-03-03 17:54:07 +0530

    done sumFibs

--- js-exercises/sum-fibs/sumFibs.js
+++ js-exercises/sum-fibs/sumFibs.js
@@ -1,5 +1,18 @@
-function sumFibs() {
+function sumFibs(num) {
+  let result = 0;
+  let a = 1;
+  let b = 1;
 
+  while (a < num) {
+    if (a % 2 !== 0) {
+      result += a;
+    }
+
+    const temp = b;
+    b = a + b;
+    a = temp;
+  }
+  return result;
 }
 
 export {
