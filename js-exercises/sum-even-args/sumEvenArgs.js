059cabae64f25f39b6778ce7f12c546aea85ee1a
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-03-06 17:31:46 +0530

    done sum even args

--- js-exercises/sum-even-args/sumEvenArgs.js
+++ js-exercises/sum-even-args/sumEvenArgs.js
@@ -1,5 +1,7 @@
 const sumEvenArgs = (...args) => {
-  return args;
+  const initialValueOfAccumulator = 0;
+  return args.reduce((accumulator, number) => (
+    number % 2 === 0 ? accumulator + number : accumulator), initialValueOfAccumulator);
 };
 
 export { sumEvenArgs };
