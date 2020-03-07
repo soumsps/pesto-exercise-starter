ccec99259b63b7e62b4ad10a26dd60632bcdefcd
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-03-07 20:40:19 +0530

    Solve: last-index-of
    Solve using (for of) and array.entries() to get array iterator, then destructuring it to [index, item]

--- js-exercises/last-index-of/lastIndexOf.js
+++ js-exercises/last-index-of/lastIndexOf.js
@@ -1,6 +1,16 @@
 
 function lastIndexOf(...args) {
-  return args;
+  const searchItem = args[0];
+  const itemsArray = args[1];
+  let itemLastOccurrenceIndex = -1;
+
+  for (const [index, item] of itemsArray.entries()) {
+    if (searchItem === item) {
+      itemLastOccurrenceIndex = index;
+    }
+  }
+
+  return itemLastOccurrenceIndex;
 }
 
 export {
