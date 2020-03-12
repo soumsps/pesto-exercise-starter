899156c2e304c43f5bee2bfc0f184ab3a399d1c2
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-03-12 19:36:21 +0530

    Solve permutation-without-repetition Solved using recursion.

--- js-exercises/permutate-without-repetitions/permutateWithoutRepetitions.test.js
+++ js-exercises/permutate-without-repetitions/permutateWithoutRepetitions.test.js
@@ -1,4 +1,4 @@
-import { permutateWithoutRepetitions } from './permutateWithoutRepetitions';
+import { permutateWithoutRepetitions } from "./permutateWithoutRepetitions";
 
 function factorial(number) {
   let result = 1;
@@ -10,68 +10,73 @@ function factorial(number) {
   return result;
 }
 
-describe('permutateWithoutRepetitions', () => {
-  it('should permutate string', () => {
-    const permutations1 = permutateWithoutRepetitions(['A']);
-    expect(permutations1).toEqual([
-      ['A'],
-    ]);
+describe("permutateWithoutRepetitions", () => {
+  it("should permutate string", () => {
+    const permutations1 = permutateWithoutRepetitions(["A"]);
+    expect(permutations1).toEqual([["A"]]);
 
-    const permutations2 = permutateWithoutRepetitions(['A', 'B']);
+    const permutations2 = permutateWithoutRepetitions(["A", "B"]);
     expect(permutations2.length).toBe(2);
     expect(permutations2).toEqual([
-      ['A', 'B'],
-      ['B', 'A'],
+      ["A", "B"],
+      ["B", "A"]
     ]);
 
-    const permutations6 = permutateWithoutRepetitions(['A', 'A']);
+    const permutations6 = permutateWithoutRepetitions(["A", "A"]);
     expect(permutations6.length).toBe(2);
     expect(permutations6).toEqual([
-      ['A', 'A'],
-      ['A', 'A'],
+      ["A", "A"],
+      ["A", "A"]
     ]);
 
-    const permutations3 = permutateWithoutRepetitions(['A', 'B', 'C']);
+    const permutations3 = permutateWithoutRepetitions(["A", "B", "C"]);
     expect(permutations3.length).toBe(factorial(3));
     expect(permutations3).toEqual([
-      ['A', 'B', 'C'],
-      ['B', 'A', 'C'],
-      ['B', 'C', 'A'],
-      ['A', 'C', 'B'],
-      ['C', 'A', 'B'],
-      ['C', 'B', 'A'],
+      ["A", "B", "C"],
+      ["A", "C", "B"],
+      ["B", "A", "C"],
+      ["B", "C", "A"],
+      ["C", "A", "B"],
+      ["C", "B", "A"]
     ]);
 
-    const permutations4 = permutateWithoutRepetitions(['A', 'B', 'C', 'D']);
+    const permutations4 = permutateWithoutRepetitions(["A", "B", "C", "D"]);
     expect(permutations4.length).toBe(factorial(4));
     expect(permutations4).toEqual([
-      ['A', 'B', 'C', 'D'],
-      ['B', 'A', 'C', 'D'],
-      ['B', 'C', 'A', 'D'],
-      ['B', 'C', 'D', 'A'],
-      ['A', 'C', 'B', 'D'],
-      ['C', 'A', 'B', 'D'],
-      ['C', 'B', 'A', 'D'],
-      ['C', 'B', 'D', 'A'],
-      ['A', 'C', 'D', 'B'],
-      ['C', 'A', 'D', 'B'],
-      ['C', 'D', 'A', 'B'],
-      ['C', 'D', 'B', 'A'],
-      ['A', 'B', 'D', 'C'],
-      ['B', 'A', 'D', 'C'],
-      ['B', 'D', 'A', 'C'],
-      ['B', 'D', 'C', 'A'],
-      ['A', 'D', 'B', 'C'],
-      ['D', 'A', 'B', 'C'],
-      ['D', 'B', 'A', 'C'],
-      ['D', 'B', 'C', 'A'],
-      ['A', 'D', 'C', 'B'],
-      ['D', 'A', 'C', 'B'],
-      ['D', 'C', 'A', 'B'],
-      ['D', 'C', 'B', 'A'],
+      ["A", "B", "C", "D"],
+      ["A", "B", "D", "C"],
+      ["A", "C", "B", "D"],
+      ["A", "C", "D", "B"],
+      ["A", "D", "B", "C"],
+      ["A", "D", "C", "B"],
+      ["B", "A", "C", "D"],
+      ["B", "A", "D", "C"],
+      ["B", "C", "A", "D"],
+      ["B", "C", "D", "A"],
+      ["B", "D", "A", "C"],
+      ["B", "D", "C", "A"],
+      ["C", "A", "B", "D"],
+      ["C", "A", "D", "B"],
+      ["C", "B", "A", "D"],
+      ["C", "B", "D", "A"],
+      ["C", "D", "A", "B"],
+      ["C", "D", "B", "A"],
+      ["D", "A", "B", "C"],
+      ["D", "A", "C", "B"],
+      ["D", "B", "A", "C"],
+      ["D", "B", "C", "A"],
+      ["D", "C", "A", "B"],
+      ["D", "C", "B", "A"]
     ]);
 
-    const permutations5 = permutateWithoutRepetitions(['A', 'B', 'C', 'D', 'E', 'F']);
+    const permutations5 = permutateWithoutRepetitions([
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F"
+    ]);
     expect(permutations5.length).toBe(factorial(6));
   });
 });
