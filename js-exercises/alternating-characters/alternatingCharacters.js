80b6d6d1816b8bd2cd232710daafc760cb4ea1de
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-03-12 15:50:55 +0530

    Solve alternating-characters Solved using functional programming, where getMinimumDeletion() loops over string and keep tracks of previous character, if matched with current character then increment the deletion counter.

--- js-exercises/alternating-characters/alternatingCharacters.js
+++ js-exercises/alternating-characters/alternatingCharacters.js
@@ -1,3 +1,18 @@
-function alternatingCharacters(arr) {}
+function getMinimumDeletion(string) {
+  let previousChar = "";
+  let deletionCount = 0;
+  string.split("").map(char => {
+    if (char === previousChar) {
+      deletionCount += 1;
+    }
+    previousChar = char;
+  });
+
+  return deletionCount;
+}
+
+function alternatingCharacters(arr) {
+  return arr.map(string => getMinimumDeletion(string));
+}
 
 export { alternatingCharacters };
