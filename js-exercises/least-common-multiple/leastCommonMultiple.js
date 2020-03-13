9be8aac1269fa21ebc53239e489a150e05b7a6ee
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-03-13 14:38:05 +0530

    Solve least-common-multiple and add tests for it Solved and added 3 test for this question

--- js-exercises/least-common-multiple/leastCommonMultiple.js
+++ js-exercises/least-common-multiple/leastCommonMultiple.js
@@ -1,8 +1,15 @@
-
 function leastCommonMultiple(...args) {
-  return args;
+  const largest = Math.max(args[0], args[1]);
+  const smallest = Math.min(args[0], args[1]);
+  let resultLCM = largest;
+
+  while (true) {
+    if (resultLCM % smallest === 0) {
+      break;
+    }
+    resultLCM += largest;
+  }
+  return resultLCM;
 }
 
-export {
-  leastCommonMultiple,
-};
+export { leastCommonMultiple };
