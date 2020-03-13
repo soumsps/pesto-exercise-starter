ebf7de379262e0aeaeab036a3f5354a2c3a564cb
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-03-13 11:53:21 +0530

    Solve update-object and write test for it. Solved and added 5 test for the same.

--- js-exercises/update-object/updateObject.js
+++ js-exercises/update-object/updateObject.js
@@ -1,8 +1,12 @@
-
 function updateObject(...args) {
-  return args;
+  let indexToReplace = args[0];
+  const providedNewElement = args[1];
+  const array = args[2];
+
+  if (indexToReplace < 0 || indexToReplace >= array.length) {
+    indexToReplace = array.length - 1;
+  }
+  return array.map((element, index) => (indexToReplace === index ? providedNewElement : element));
 }
 
-export {
-  updateObject,
-};
+export { updateObject };
--- js-exercises/update-object/updateObject.test.js
+++ js-exercises/update-object/updateObject.test.js
@@ -1,3 +1,36 @@
-import { updateObject } from "./updateObject";
+import { updateObject } from './updateObject';
 
-describe("updateObject", () => {});
+describe('updateObject', () => {
+  test('should return new array', () => {
+    const array = ['a', 'b', 'c'];
+    const newArray = updateObject(1, '_', array);
+    expect(newArray).not.toBe(array);
+  });
+
+  test('should return object of Array type', () => {
+    const array = ['a', 'b', 'c'];
+    const newArray = updateObject(1, '_', array);
+    expect(Array.isArray(newArray)).toBe(true);
+  });
+
+  test('when replacement index is less than 0', () => {
+    const array = ['a', 'b', 'c'];
+    const newArray = updateObject(-1, '_', array);
+    expect(newArray).toEqual(['a', 'b', '_']);
+  });
+
+  test('when replacement index is greater than or equal to length of passed array', () => {
+    const array = ['a', 'b', 'c'];
+    const newArray1 = updateObject(3, '_', array);
+    expect(newArray1).toEqual(['a', 'b', '_']);
+
+    const newArray2 = updateObject(4, '_', array);
+    expect(newArray2).toEqual(['a', 'b', '_']);
+  });
+
+  test('should return new array with replacement at index 0', () => {
+    const array = ['c', 'b'];
+    const newArray = updateObject(0, 'a', array);
+    expect(newArray).toEqual(['a', 'b']);
+  });
+});
