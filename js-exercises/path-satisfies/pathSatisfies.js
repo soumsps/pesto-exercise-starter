6c3c3caa6171f91d64df15f3e3a912e25926ee80
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-03-14 21:32:31 +0530

    Solve path-satisfies

--- js-exercises/path-satisfies/pathSatisfies.js
+++ js-exercises/path-satisfies/pathSatisfies.js
@@ -1,8 +1,19 @@
-
-function pathSatisfies(...args) {
-  return args;
+function getPathData(path, dataObj) {
+  return dataObj[path];
 }
 
-export {
-  pathSatisfies,
-};
+function pathSatisfies(...args) {
+  const condition = args[0];
+  const paths = args[1];
+  const dataObject = args[2];
+  let pathResult = dataObject;
+
+  // eslint-disable-next-line array-callback-return
+  paths.map(path => {
+    pathResult = getPathData(path, pathResult);
+  });
+
+  return condition(pathResult);
+}
+
+export { pathSatisfies };
