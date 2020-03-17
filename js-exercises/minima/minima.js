5a6e3aba0456266ee7b74d5e3c2467b80ec5fa5e
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-03-17 19:49:09 +0530

    Solve minima This algorithm needs only single scan of given array.

--- js-exercises/minima/minima.js
+++ js-exercises/minima/minima.js
@@ -1,3 +1,16 @@
-function minima() {}
+function minima(n, arr) {
+  const resultArr = new Array(n).fill(Number.MAX_VALUE);
+
+  for (const num of arr) {
+    for (let i = 0; i < resultArr.length; i += 1) {
+      if (num < resultArr[i]) {
+        resultArr.splice(i, 0, num);
+        break;
+      }
+    }
+  }
+
+  return resultArr.slice(0, n);
+}
 
 export { minima };
