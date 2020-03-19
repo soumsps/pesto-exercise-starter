64331251d2464382753544409bf2c78ff1018d77
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-03-19 16:55:05 +0530

    Solve freq-sort Solved using function composition

--- js-exercises/freq-sort/freqSort.js
+++ js-exercises/freq-sort/freqSort.js
@@ -1,8 +1,28 @@
+const createCharacterFrequencyMap = arr => {
+  const frequencyMap = new Map();
+  arr.map(character => {
+    if (frequencyMap.has(character)) {
+      frequencyMap.set(character, frequencyMap.get(character) + 1);
+    } else {
+      frequencyMap.set(character, 1);
+    }
+  });
 
-function freqSort(...args) {
-  return args;
+  return frequencyMap;
+};
+
+const generateResultArray = arr => arr.map(el => el[0]);
+const toArray = Array.from;
+const sortByValue = arr => arr.sort((el1, el2) => el2[1] - el1[1]);
+const compose = (...fns) => initialVal => fns.reduceRight((val, func) => func(val), initialVal);
+
+function freqSort(arrayOfCharacters) {
+  return compose(
+    generateResultArray,
+    sortByValue,
+    toArray,
+    createCharacterFrequencyMap
+  )(arrayOfCharacters);
 }
 
-export {
-  freqSort,
-};
+export { freqSort };
--- js-exercises/freq-sort/freqSort.test.js
+++ js-exercises/freq-sort/freqSort.test.js
@@ -1,7 +1,15 @@
 import { freqSort } from './freqSort';
 
-describe('Template Test', () => {
-  test('Template Test', () => {
-    expect(true).toBe('tests for freqSort not written');
+describe('Frequency Sort', () => {
+  test('Test 1', () => {
+    expect(freqSort(['a', 'z', 'z', 'z', 'b', 'b', 'z'])).toEqual(['z', 'b', 'a']);
+  });
+
+  test('Test 2', () => {
+    expect(freqSort(['a', 'a', 'z', 'z', 'b', 'b'])).toEqual(['a', 'z', 'b']);
+  });
+
+  test('When input array is empty', () => {
+    expect(freqSort([])).toEqual([]);
   });
 });
