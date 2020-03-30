d0942bda3867fc419f646d037c6ee2b26e0f4094
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-03-30 10:24:10 +0530

    WIP on-change Still some testcases are not passing, will need to devote some more time in future.

--- js-exercises/on-change/onChange.js
+++ js-exercises/on-change/onChange.js
@@ -1,3 +1,42 @@
-const onChange = (obj, onChangeFn) => {};
+const onChange = (objToWatch, onChangeFunction) => {
+  const handler = {
+    get(target, property, receiver) {
+      onChangeFunction(); // Calling our function
+      const value = Reflect.get(target, property, receiver);
+      if (typeof value === 'object') {
+        return new Proxy(value, handler);
+      }
+      return value;
+    },
+
+    // set(target, property, value, receiver) {
+    //   onChangeFunction();
+    //   return Reflect.set(target, property, value, receiver);
+    // },
+
+    defineProperty(target, property, descriptor) {
+      const value = Reflect.defineProperty(target, property, descriptor);
+      if (typeof value === 'object') {
+        return new Proxy(value, handler);
+      }
+
+      const oldDescriptor = Object.getOwnPropertyDescriptor(target, property);
+      Reflect.defineProperty(target, property, {
+        ...oldDescriptor,
+        ...descriptor
+      });
+
+      onChangeFunction();
+      return true;
+    },
+
+    deleteProperty(target, property) {
+      onChangeFunction();
+      return Reflect.deleteProperty(target, property);
+    }
+  };
+
+  return new Proxy(objToWatch, handler);
+};
 
 export { onChange };
