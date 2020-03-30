15a61e3aa191fcc3efefe6804c19e9d35d5094bc
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-03-30 11:19:32 +0530

    Solve private-props

--- js-exercises/private-props/privateProps.js
+++ js-exercises/private-props/privateProps.js
@@ -1,3 +1,20 @@
-function privateProps(obj, isPrivate) {}
+function privateProps(obj, isPrivate) {
+  const handler = {
+    set(target, property, value, receiver) {
+      if (isPrivate(property)) {
+        throw new TypeError(`Can't set property "_private"`);
+      } else {
+        Reflect.set(target, property, value, receiver);
+      }
+    },
+    has(target, property) {
+      return property in target && !isPrivate(property);
+    },
+    ownKeys(target) {
+      return Reflect.ownKeys(target).filter(key => !isPrivate(key));
+    }
+  };
+  return new Proxy(obj, handler);
+}
 
 export { privateProps };
