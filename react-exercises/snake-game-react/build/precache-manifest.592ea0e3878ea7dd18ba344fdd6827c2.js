389154b00f77e9a49a5a43ab9900b9f611585f07
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-04-06 19:32:26 +0530

    Update build files.

---
+++ react-exercises/snake-game-react/build/precache-manifest.592ea0e3878ea7dd18ba344fdd6827c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b0f70a9729350afc99b9bfaec074a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b586ad7239ccbfbf1d87",
+    "url": "/static/css/main.d0f16cc3.chunk.css"
+  },
+  {
+    "revision": "b43b9f0081f5ada66034",
+    "url": "/static/js/2.dfddc9b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dfddc9b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b586ad7239ccbfbf1d87",
+    "url": "/static/js/main.839fbdd3.chunk.js"
+  },
+  {
+    "revision": "6a576719b365362edf12",
+    "url": "/static/js/runtime-main.fe76298c.js"
+  }
+]);
--- react-exercises/snake-game-react/build/service-worker.js
+++ react-exercises/snake-game-react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.faba3828d034d6eda9595ed192a5cb18.js"
+  "/precache-manifest.592ea0e3878ea7dd18ba344fdd6827c2.js"
 );
 
 self.addEventListener('message', (event) => {
